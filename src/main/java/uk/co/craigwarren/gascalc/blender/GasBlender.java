475e6deba290852c4e646f5b9720a1443932ca4a
Craig Warren <craig.warren@swiftkey.net>
2013-09-26 13:45:30 +0100

    Adding interface for Gas Blenders

--- src/main/java/uk/co/craigwarren/gascalc/blender/GasBlender.java
+++ src/main/java/uk/co/craigwarren/gascalc/blender/GasBlender.java
@@ -3,13 +3,24 @@
  */
 package uk.co.craigwarren.gascalc.blender;
 
+import uk.co.craigwarren.gascalc.model.FillRequirement;
+import uk.co.craigwarren.gascalc.model.filling.FillPlan;
+
 /**
- * Interface for GasBlenders used to blending gas in empty cylinders
+ * Abstract implementation for GasBlenders used to blending gas in empty cylinders
  * or gas topups to partially filled cylinders
  * 
  * @author craig
  *
  */
 public interface GasBlender {
+    
+    /**
+     * Generate the plan for how to acieve a fill requirement
+     * 
+     * @param requirement The required gas, and cylinder information
+     * @return the steps to achieve the fill
+     */
+    FillPlan blendGas(FillRequirement requirement);
 
 }
