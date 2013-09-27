7cdc7adf401e667c96ffbfb192c00d3a571a27c0
Craig Warren <craig.warren@swiftkey.net>
2013-09-27 16:50:50 +0100

    Adding implementation of GasLawBlender and it's int test. Work Remains

---
+++ src/main/java/uk/co/craigwarren/gascalc/blender/GasLawBlender.java
@@ -0,0 +1,251 @@
+/**
+ * 
+ */
+package uk.co.craigwarren.gascalc.blender;
+
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+import uk.co.craigwarren.gascalc.exception.ImpossibleMixException;
+import uk.co.craigwarren.gascalc.gaslaw.GasLaw;
+import uk.co.craigwarren.gascalc.model.Cylinder;
+import uk.co.craigwarren.gascalc.model.Element;
+import uk.co.craigwarren.gascalc.model.FillRequirement;
+import uk.co.craigwarren.gascalc.model.Gas;
+import uk.co.craigwarren.gascalc.model.filling.FillAction;
+import uk.co.craigwarren.gascalc.model.filling.FillPlan;
+import uk.co.craigwarren.gascalc.model.filling.FillStep;
+
+/**
+ * A Gas Blender which uses a gas law to calculate Fill Plans
+ * 
+ * @author craig
+ *
+ */
+public class GasLawBlender implements GasBlender {
+
+    private GasLaw gasLaw;
+    
+    public GasLawBlender(GasLaw law) {
+        if(null == law) {
+            throw new IllegalArgumentException("GasLaw law cannot be null");
+        }
+        gasLaw = law;
+    }
+
+    public FillPlan blendGas(FillRequirement requirement) throws ImpossibleMixException {
+        Map<Element,Double> targetMolByElement = getTargetMolsByElement(requirement);
+        Map<Element,Double> availableMolsByElement = getAvailableMolsByElement(requirement.getStartingCylinder());
+        Map<Element,Double> requiredMolsByElement = 
+                subtractMolsByElement(targetMolByElement, availableMolsByElement);
+        
+        FillStep drain = getDrainAction(requirement, requiredMolsByElement, availableMolsByElement, targetMolByElement);
+        if(null != drain) {
+            //OK so we need to drain the cylinder first and then generate a plan based on the new starting pressure
+            requirement.getStartingCylinder().setPressure(drain.getPressure());
+            FillPlan plan = blendGas(requirement);
+            plan.addFirst(drain);
+            return plan;
+        }
+        
+        requiredMolsByElement = removeOxygenFromAir(requiredMolsByElement, requirement.getTargetGas());
+        
+        FillPlan plan = new FillPlan();
+        Cylinder cylinder = requirement.getStartingCylinder();
+        
+        if(requiredMolsByElement.containsKey(Element.HELIUM) && requiredMolsByElement.get(Element.HELIUM) > 0.0){
+            plan.add(addGasToCylinder(cylinder, Element.HELIUM, requiredMolsByElement.get(Element.HELIUM)));
+        }
+        if(requiredMolsByElement.containsKey(Element.OXYGEN) && requiredMolsByElement.get(Element.OXYGEN) > 0.0) {
+            plan.add(addGasToCylinder(cylinder, Element.OXYGEN, requiredMolsByElement.get(Element.OXYGEN)));
+        }
+        plan.add(new FillStep(FillAction.FILL, requirement.getTargetPressure(), Gas.air()));
+        
+        return plan;
+    }
+    
+    /**
+     * Adds the required element to the cylinder and returns a fill step indicating the action to take
+     * 
+     * @param cylinder
+     * @param element
+     * @param mols
+     * @return
+     */
+    private FillStep addGasToCylinder(Cylinder cylinder, Element element, double molsToAdd) {
+        Map<Element, Double> molsByElement = getAvailableMolsByElement(cylinder);
+        double currentMols = 0.0;
+        if(molsByElement.containsKey(element)) {
+            currentMols = molsByElement.get(element);
+        }
+        currentMols = currentMols + molsToAdd;
+        molsByElement.put(element, currentMols);
+        Gas newGas = new Gas(molsByElement);
+        gasLaw.setGas(newGas);
+        double newPressure = gasLaw.getPressureBar(cylinder.getVolume(), getTotalAmount(molsByElement), cylinder.getAmbientTemperatureKelvin());
+        FillStep step = new FillStep(FillAction.FILL, newPressure, new Gas(element));
+        cylinder.setGas(newGas);
+        cylinder.setPressure(newPressure);
+        
+        return step;
+    }
+    
+    /**
+     * @param molsByElement the collection of mols separated by element
+     * @return the sum of all mols
+     */
+    private double getTotalAmount(Map<Element,Double> molsByElement) {
+        double sum = 0.0;
+        for (Double amount : molsByElement.values()) {
+            sum = sum + amount;
+        }
+        return sum;
+    }
+    
+    private Map<Element,Double> removeOxygenFromAir(Map<Element,Double> molsByElement, Gas targetGas) throws ImpossibleMixException {
+        if(molsByElement.containsKey(Element.NITROGEN)) {
+            double freeOxygen = getMolsOfOxygenFromAir(molsByElement.get(Element.NITROGEN));
+            if(molsByElement.containsKey(Element.OXYGEN)) {
+                double oxygenMols = molsByElement.get(Element.OXYGEN);
+                oxygenMols = oxygenMols - freeOxygen;
+                if(oxygenMols < 0.0) {
+                    throw new ImpossibleMixException(targetGas);
+                }
+                molsByElement.put(Element.OXYGEN, oxygenMols);
+            } else {
+                throw new ImpossibleMixException(targetGas);
+            }
+        }
+        return molsByElement;
+    }
+    
+    private FillStep getDrainAction(FillRequirement requirement, Map<Element,Double> requiredMolsByElement, Map<Element,Double> availableMolsByElement, Map<Element,Double> targetMolsByElement) {
+        //TODO: Work out how to do more effitient draining
+                
+        Map<Element,Double> negativeMols = Maps.newEnumMap(Element.class);
+        double molsOfFreeOxygen = 0.0;
+        
+        if(targetMolsByElement.containsKey(Element.NITROGEN)) {
+            molsOfFreeOxygen = getMolsOfOxygenFromAir(targetMolsByElement.get(Element.NITROGEN));
+
+        }
+        
+        for (Element element : requiredMolsByElement.keySet()) {
+            double molsRequired = requiredMolsByElement.get(element);
+            if(element == Element.OXYGEN) {
+                molsRequired = molsRequired - molsOfFreeOxygen;
+            }
+            if(molsRequired < 0.0) {
+                negativeMols.put(element, molsRequired);
+            }
+        }
+        
+        if(negativeMols.isEmpty()) {
+            return null;
+        }
+        /*
+        double maxReductionRatio = 0.0;
+        Element maxReductionElement = Element.NITROGEN;
+        
+        for (Element element : negativeMols.keySet()) {
+            double amountToReduce = Math.abs(negativeMols.get(element));
+            double amountAvailable = availableMolsByElement.get(element);
+            double ratio = amountToReduce/amountAvailable;
+            if (ratio > maxReductionRatio) {
+                maxReductionRatio = ratio;
+                maxReductionElement = element;
+            }
+        }
+        
+        double newAvailableMols = 0.0;
+        for (Element element : availableMolsByElement.keySet()) {
+            double availableMols = availableMolsByElement.get(element); 
+            double elementRatio = requirement.getStartingCylinder().getGas().getDecimalRatio(element) / requirement.getStartingCylinder().getGas().getDecimalRatio(maxReductionElement);
+            double newMols = availableMols - (availableMols * maxReductionRatio * elementRatio);
+            newAvailableMols = newAvailableMols + newMols;
+        }
+        
+        gasLaw.setGas(requirement.getStartingCylinder().getGas());
+        double reducedPressure = gasLaw.getPressureBar(requirement.getStartingCylinder().getVolume(), newAvailableMols, requirement.getStartingCylinder().getAmbientTemperatureKelvin());
+        */    
+        return new FillStep(FillAction.DRAIN, 0.0, requirement.getStartingCylinder().getGas());
+        
+    }
+    
+    private double getMolsOfOxygenFromAir(Double molsOfNitrogen) {
+        return (molsOfNitrogen/Gas.air().getDecimalRatio(Element.NITROGEN))*Gas.air().getDecimalRatio(Element.OXYGEN);
+    }
+    
+    /**
+     * Performs subtraction of doubles between two maps, but only subtract doubles where they have matching key elements
+     * *Note: elements in the subtrahend but not the minuend will result in a negative number
+     * 
+     * @param minuend the Map of doubles to subtract from
+     * @param subtrahend the map of doubles to subtract
+     * @return a map containing the minuend doubles minus the subrahend doubles
+     */
+    private Map<Element, Double> subtractMolsByElement(Map<Element,Double> minuend, Map<Element,Double> subtrahend) {
+        Map<Element,Double> result = Maps.newEnumMap(Element.class);
+        for (Element minuendElement : minuend.keySet()) {
+            double minuendMols = minuend.get(minuendElement);
+            double resultMols = minuendMols;
+            if(subtrahend.containsKey(minuendElement)) {
+                resultMols = minuendMols - subtrahend.get(minuendElement);
+            }
+            result.put(minuendElement, resultMols);
+        }
+        for (Element subtrahendElement : subtrahend.keySet() ) {
+            if(!result.containsKey(subtrahendElement)) {
+                result.put(subtrahendElement, 0.0 - subtrahend.get(subtrahendElement));
+            }
+        }
+        return result;
+    }
+    
+    private Map<Element, Double> getTargetMolsByElement(FillRequirement requirement) throws ImpossibleMixException {
+        Map<Element, Double> result = getMolsByElement(requirement.getTargetGas(), getTargetMols(requirement));
+        checkForImpossibleMix(requirement.getTargetGas(),result);
+        return result;
+    }
+    
+    private  Map<Element, Double> getAvailableMolsByElement(Cylinder cylinder) {
+        return getMolsByElement(cylinder.getGas(), getAvailableMols(cylinder));
+    }
+    
+    private double getAvailableMols(Cylinder cylinder) {
+        gasLaw.setGas(cylinder.getGas());
+        return gasLaw.getMolsOfGas(cylinder.getPressure(), cylinder.getVolume(), cylinder.getAmbientTemperatureKelvin());
+    }
+    
+    private double getTargetMols(FillRequirement requirement) {
+        gasLaw.setGas(requirement.getTargetGas());
+        return gasLaw.getMolsOfGas(requirement.getTargetPressure(), requirement.getStartingCylinder().getVolume(), requirement.getStartingCylinder().getAmbientTemperatureKelvin());
+    }
+
+    
+    private Map<Element, Double> getMolsByElement(Gas gas, double mols) {
+        Map<Element,Double> result = Maps.newEnumMap(Element.class);
+        for (Element element : gas.getComponents()) {
+            result.put(element, gas.getDecimalRatio(element)*mols);
+        }
+        return result;
+    }
+    
+private void checkForImpossibleMix(Gas gas, Map<Element, Double> targetMolsByElement) throws ImpossibleMixException {
+        if( targetMolsByElement.containsKey(Element.NITROGEN)) {
+            double nitrogenMolsRequired = targetMolsByElement.get(Element.NITROGEN);
+            double oxygenMolsFree = getMolsOfOxygenFromAir(nitrogenMolsRequired);
+            double oxygenMolsRequired = 0.0;
+            
+            if(targetMolsByElement.containsKey(Element.OXYGEN)) {
+                oxygenMolsRequired = targetMolsByElement.get(Element.OXYGEN);
+            }
+            
+            if(oxygenMolsRequired < oxygenMolsFree){
+                throw new ImpossibleMixException(gas);
+            }
+            
+        }
+    }
+}
---
+++ src/test/java/uk/co/craigwarren/gascalc/blender/GasLawBlenderIntTest.java
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+package uk.co.craigwarren.gascalc.blender;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import uk.co.craigwarren.gascalc.UnitConverter;
+import uk.co.craigwarren.gascalc.exception.ImpossibleMixException;
+import uk.co.craigwarren.gascalc.gaslaw.IdealGasLaw;
+import uk.co.craigwarren.gascalc.model.Cylinder;
+import uk.co.craigwarren.gascalc.model.Element;
+import uk.co.craigwarren.gascalc.model.FillRequirement;
+import uk.co.craigwarren.gascalc.model.Gas;
+import uk.co.craigwarren.gascalc.model.filling.FillAction;
+import uk.co.craigwarren.gascalc.model.filling.FillPlan;
+import uk.co.craigwarren.gascalc.model.filling.FillStep;
+
+/**
+ * Integration Test for the gas law blender
+ * 
+ * @author craig
+ *
+ */
+public class GasLawBlenderIntTest {
+    
+    @Test
+    public void testNitroxMixWithIdealGasLaws() throws ImpossibleMixException{
+        GasBlender blender = new GasLawBlender(new IdealGasLaw());
+        FillRequirement requirement = new FillRequirement(new Cylinder(12), 230.0, new Gas(30,70));
+        FillPlan plan = blender.blendGas(requirement);
+        Assert.assertEquals("There should be two steps in the plan",2,plan.size());
+        FillStep step1 = plan.get(0);
+        Assert.assertEquals("The first step should be a fill",FillAction.FILL,step1.getAction());
+        Assert.assertEquals("The First action should use Oxygen", new Gas(Element.OXYGEN),step1.getGas());
+        Assert.assertEquals("The First step pressure is incorrect", 26.2, step1.getPressure(),0.1);
+        FillStep step2 = plan.get(1);
+        Assert.assertEquals("The second step should be a fill",FillAction.FILL,step2.getAction());
+        Assert.assertEquals("The second action should use Air", Gas.air(),step2.getGas());
+        Assert.assertEquals("The second step pressure is incorrect",230.0, step2.getPressure(),0.1);
+        
+    }
+    
+    @Test(expected=ImpossibleMixException.class)
+    public void testHypoxicMixWithIdealGasLaws() throws ImpossibleMixException {
+        GasBlender blender = new GasLawBlender(new IdealGasLaw());
+        FillRequirement requirement = new FillRequirement(new Cylinder(12), 230.0, new Gas(20,80));
+        blender.blendGas(requirement);
+    }
+    
+    @Test
+    public void testTopUpNitroxWithIdealGasLaws() throws ImpossibleMixException {
+        GasBlender blender = new GasLawBlender(new IdealGasLaw());
+        FillRequirement requirement = new FillRequirement(new Cylinder(12.0,120.0), 230.0, new Gas(30,70));
+        FillPlan plan = blender.blendGas(requirement);
+        Assert.assertEquals("There should be two steps in the plan",2,plan.size());
+        FillStep step1 = plan.get(0);
+        Assert.assertEquals("The first step should be a fill",FillAction.FILL,step1.getAction());
+        Assert.assertEquals("The First action should use Oxygen", new Gas(Element.OXYGEN),step1.getGas());
+        Assert.assertEquals("The First step pressure is incorrect", 146.2, step1.getPressure(),0.1);
+        FillStep step2 = plan.get(1);
+        Assert.assertEquals("The second step should be a fill",FillAction.FILL,step2.getAction());
+        Assert.assertEquals("The second action should use Air", Gas.air(),step2.getGas());
+        Assert.assertEquals("The second step pressure is incorrect",230.0, step2.getPressure(),0.1);
+        
+    }
+    
+    @Test
+    public void testDrainFirstNitroxWithIdealGasLaws() throws ImpossibleMixException {
+        GasBlender blender = new GasLawBlender(new IdealGasLaw());
+        FillRequirement requirement = new FillRequirement(new Cylinder(12.0,200.0,new Gas(40,60),UnitConverter.celciusToKelvin(20)), 230.0, new Gas(30,70));
+        FillPlan plan = blender.blendGas(requirement);
+        Assert.assertEquals("There should be three steps in the plan",3,plan.size()); 
+        FillStep step1 = plan.get(0);
+        Assert.assertEquals("The first step should be a Drain",FillAction.DRAIN,step1.getAction());
+        Assert.assertEquals("The First step pressure is incorrect", 0.0, step1.getPressure(),0.1);
+        
+        FillStep step2 = plan.get(1);
+        Assert.assertEquals("The second step should be a fill",FillAction.FILL,step2.getAction());
+        Assert.assertEquals("The second action should use Oxygen", new Gas(Element.OXYGEN),step2.getGas());
+        Assert.assertEquals("The second step pressure is incorrect", 26.2, step2.getPressure(),0.1);
+        
+        FillStep step3 = plan.get(2);
+        Assert.assertEquals("The third step should be a fill",FillAction.FILL,step3.getAction());
+        Assert.assertEquals("The third action should use Air", Gas.air(),step3.getGas());
+        Assert.assertEquals("The third step pressure is incorrect",230.0, step3.getPressure(),0.1);
+
+    }
+
+}
