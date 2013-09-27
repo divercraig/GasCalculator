b483ddfbe1e6b9ecdadfe9a9f5d9d6765088838f
Craig Warren <craig.warren@swiftkey.net>
2013-09-27 16:49:36 +0100

    Gas Blender now throws Impossible Mix exception

--- src/main/java/uk/co/craigwarren/gascalc/blender/GasBlender.java
+++ src/main/java/uk/co/craigwarren/gascalc/blender/GasBlender.java
@@ -3,6 +3,7 @@
  */
 package uk.co.craigwarren.gascalc.blender;
 
+import uk.co.craigwarren.gascalc.exception.ImpossibleMixException;
 import uk.co.craigwarren.gascalc.model.FillRequirement;
 import uk.co.craigwarren.gascalc.model.filling.FillPlan;
 
@@ -20,7 +21,8 @@ public interface GasBlender {
      * 
      * @param requirement The required gas, and cylinder information
      * @return the steps to achieve the fill
+     * @throws ImpossibleMixException if the gas cannot be mixed without pure nitrogen
      */
-    FillPlan blendGas(FillRequirement requirement);
+    FillPlan blendGas(FillRequirement requirement) throws ImpossibleMixException;
 
 }
